1fc520c43ba36e231962a7d6b78ec56ba4b3d46b
Likitha Khatri <likithakhatri5774@gmail.com>
2024-02-11 16:30:27 +0530

    My Portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <div style={{ height: '2000px' }}></div>
+    </div>
+  );
+};
+
+export default App;
